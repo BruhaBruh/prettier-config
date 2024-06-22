eab77bbcf00cd34b81f3d140a14f3c41e1780475
BruhaBruh <drugsho.jaker@gmail.com>
2024-06-22 13:42:37 +0500

    feat: initial

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  endOfLine: 'lf',
+  bracketSpacing: false,
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+  ],
+};
